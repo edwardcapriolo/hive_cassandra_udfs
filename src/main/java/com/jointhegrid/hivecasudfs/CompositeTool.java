8436180f3015f57f275ca939cc857c6251cb33d7
Edward Capriolo <edlinuxguru@gmail.com>
2012-03-29 20:59:14 -0400

    These should work but I do not have units for them yet

---
+++ src/main/java/com/jointhegrid/hivecasudfs/CompositeTool.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jointhegrid.hivecasudfs;
+
+/**
+ *
+ * @author edward
+ */
+public class CompositeTool {
+
+}
